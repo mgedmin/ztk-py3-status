dc569f572b5b99d8ae977b0238ae450a2bbce613
Marius Gedminas <marius@gedmin.as>
2013-01-30 18:47:15 +0200

    New option: get_deps.py --cache-dir (and also --help)

--- README.rst
+++ README.rst
@@ -3,13 +3,27 @@ Scripts to determine the Python 3 porting status of various Zope packages
 
 Usage::
 
-  ./get_zope_packages.py | ./get_pypi_status.py > status.json
+  ./get_zope_packages.py > packages.json
+  ./get_pypi_status.py < packages.json > status.json
+  ./get_deps.py < status.json > deps.json
 
-This takes a while (8 minutes for me).
+This takes a while (8 minutes just to get PyPI status; more to download
+source distributions).
 
 Example output::
 
   [{"name": "zope.interface",
     "version": "4.0.3",
     "supports": ["2.6", "2.7", "3.2", "3.3"]},
+    "requires": ["setuptools"],
    ...]
+
+
+Caching
+-------
+
+The sdist cache used by get_deps.py is (a) configurable, and (b) compatible
+with buildout.  If you use a shared buildout cache, so you can speed up
+the initial dependency extraction with ::
+
+  ./get_deps --cache-dir=~/.buildout/cache/dist < status.json > deps.json
--- get_deps.py
+++ get_deps.py
@@ -17,6 +17,7 @@ distributions, which have to be downloaded from the Internet.
 This script requires Python 3.
 """
 
+import argparse
 import json
 import os
 import sys
@@ -24,9 +25,6 @@ import tarfile
 import zipfile
 from urllib.parse import urlparse
 from urllib.request import urlretrieve
-
-
-CACHE_DIR = os.path.expanduser('~/.buildout/cache/dist') # XXX hardcoded to my prefs
 
 
 class Error(Exception):
@@ -42,18 +40,18 @@ class Error(Exception):
 #  - XXX decide what to do with extras information
 
 
-def get_cache_filename(sdist_url):
+def get_cache_filename(sdist_url, cache_dir):
     """Compute the pathname of the cache file corresponding to sdist_url."""
     basename = os.path.basename(urlparse(sdist_url).path)
-    return os.path.join(CACHE_DIR, basename)
+    return os.path.join(cache_dir, basename)
 
 
-def get_local_sdist(sdist_url):
+def get_local_sdist(sdist_url, cache_dir):
     """Return the filename corresponding to a source distribution.
 
     Downloads the file from sdist_url into the cache directory if necessary.
     """
-    filename = get_cache_filename(sdist_url)
+    filename = get_cache_filename(sdist_url, cache_dir)
     if not os.path.exists(filename):
         # This would be a good spot for a "Downloading {}" message if verbose
         urlretrieve(sdist_url, filename)
@@ -142,6 +140,18 @@ def dump_pretty_json(data, fp=sys.stdout):
 
 
 def main():
+    parser = argparse.ArgumentParser(
+        description='annotate package JSON with requirements',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--cache-dir', metavar='DIR', default='.cache',
+                        help='directory for caching downloaded sdists')
+    args = parser.parse_args()
+    if not os.path.isdir(args.cache_dir):
+        try:
+            os.makedirs(args.cache_dir)
+        except Exception as e:
+            parser.error('Could not create cache directory: {}: {}'.format(
+                         e.__class__.__name__, e))
     packages = json.load(sys.stdin)
     for info in packages:
         package_name = info['name']
@@ -150,7 +160,7 @@ def main():
             continue
         requirements = []
         try:
-            sdist_filename = get_local_sdist(sdist_url)
+            sdist_filename = get_local_sdist(sdist_url, args.cache_dir)
         except Exception as e:
             print('Could not fetch sdist {}: {}: {}'.format(
                         sdist_url, e.__class__.__name__, e),
