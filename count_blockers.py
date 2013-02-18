655fcb062be011d1cac116974ce537ef96fad9d5
Marius Gedminas <marius@gedmin.as>
2013-02-18 19:36:52 +0200

    Count blockers for setuptools extras

--- count_blockers.py
+++ count_blockers.py
@@ -45,14 +45,29 @@ def main():
             info['supports_py3'] = False
             info['blockers'] = [pkg for pkg in info.get('requires') or []
                                 if pkg in do_not_support_py3]
+            info['blockers_extras'] = [
+                pkg
+                for extra, reqs in info.get('requires_extras', {}).items()
+                for pkg in reqs
+                if pkg in do_not_support_py3]
+            info['all_blockers'] = sorted(set(info['blockers'])
+                                          | set(info['blockers_extras']))
         else:
             info['supports_py3'] = True
             info['blockers'] = []
+            info['blockers_extras'] = []
+            info['all_blockers'] = []
         info['blocks'] = []
+        info['blocks_extras'] = []
+        info['blocks_all'] = []
     package_by_name = {info['name']: info for info in packages}
     for info in packages:
         for blocker in info['blockers']:
             package_by_name[blocker]['blocks'].append(info['name'])
+        for blocker in info['blockers_extras']:
+            package_by_name[blocker]['blocks_extras'].append(info['name'])
+        for blocker in info['all_blockers']:
+            package_by_name[blocker]['blocks_all'].append(info['name'])
     dump_pretty_json(packages)
 
 
